9b74702139efd52904e3fc845f520db9b5cb0f4a
F4R1x <ibmecfagundes@gmail.com>
2023-06-05 11:28:21 -0300

    Final

--- exemplointent/app/src/main/java/com/example/exemplo_intent/resultado.kt
+++ exemplointent/app/src/main/java/com/example/exemplo_intent/resultado.kt
@@ -1,6 +1,7 @@
 package com.example.exemplo_intent
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
@@ -22,99 +23,105 @@ class resultado : AppCompatActivity() {
         if(Variaveis.potencia == 5){
             if(Variaveis.beleza == 1){
                 textoresposta.text = "Ferrari 458"
-                imagemresposta.setImageResource(R.drawable.Ferrari)
+                imagemresposta.setImageResource(R.drawable.ferrari)
             }
             else{
                 textoresposta.text = "Lamborghini Gallardo"
-                imagemresposta.setImageResource(R.drawable.Lambo)
+                imagemresposta.setImageResource(R.drawable.lambo)
             }
         }
         if(Variaveis.potencia == 4){
                 textoresposta.text = "Lexus LFA"
-            imagemresposta.setImageResource(R.drawable.Lexus)
+            imagemresposta.setImageResource(R.drawable.lexus)
         }
         if(Variaveis.potencia == 3){
             if(Variaveis.beleza >= 2){
                 textoresposta.text = "Golf GTI"
-                imagemresposta.setImageResource(R.drawable.Golf)
+                imagemresposta.setImageResource(R.drawable.golf)
             }
             else if(Variaveis.custo_beneficio >= 2){
                 textoresposta.text = "Up TSI"
-                imagemresposta.setImageResource(R.drawable.Up)
+                imagemresposta.setImageResource(R.drawable.up)
             }
             else if(Variaveis.ze_da_manga >= 2){
                 textoresposta.text = "Renault Megane RS"
-                imagemresposta.setImageResource(R.drawable.Megane)
+                imagemresposta.setImageResource(R.drawable.megane)
             }
             else{
                 textoresposta.text = "Onix RS"
-                imagemresposta.setImageResource(R.drawable.Onix)
+                imagemresposta.setImageResource(R.drawable.onix)
             }
         }
         if(Variaveis.potencia == 2){
             if(Variaveis.custo_beneficio >= 3){
                 textoresposta.text = "Sandero RS"
-                imagemresposta.setImageResource(R.drawable.Sandero)
+                imagemresposta.setImageResource(R.drawable.sandero)
             }
             else if(Variaveis.ze_da_manga >= 3){
                 textoresposta.text = "Marea turbo"
-                imagemresposta.setImageResource(R.drawable.Marea)
+                imagemresposta.setImageResource(R.drawable.marea)
             }
             else if(Variaveis.arraza_role >= 3){
                 textoresposta.text = "Essa coisa aqui..."
-                imagemresposta.setImageResource(R.drawable.Coisa)
+                imagemresposta.setImageResource(R.drawable.coisa)
             }
             else{
                 textoresposta.text = "Peugeot 2008"
-                imagemresposta.setImageResource(R.drawable.P2008)
+                imagemresposta.setImageResource(R.drawable.p2008)
             }
         }
         if(Variaveis.potencia == 1){
             if(Variaveis.custo_beneficio >= 4){
                 textoresposta.text = "Renault Kwid"
-                imagemresposta.setImageResource(R.drawable.Kwid)
+                imagemresposta.setImageResource(R.drawable.kwid)
             }
             else if(Variaveis.ze_da_manga >= 4){
                 textoresposta.text = "Ta virando zé da manga..."
-                imagemresposta.setImageResource(R.drawable.Manga)
+                imagemresposta.setImageResource(R.drawable.manga)
             }
             else if(Variaveis.beleza >= 4){
                 textoresposta.text = "Jaguar F PACE"
-                imagemresposta.setImageResource(R.drawable.Jaguar)
+                imagemresposta.setImageResource(R.drawable.jaguar)
             }
             else if(Variaveis.arraza_role >= 4){
                 textoresposta.text = "Peugeot 206 TUNADO"
-                imagemresposta.setImageResource(R.drawable.Ferrari)
+                imagemresposta.setImageResource(R.drawable.p206)
             }
             else{
                 textoresposta.text = "Jeep RENEGADO"
-                imagemresposta.setImageResource(R.drawable.Jeep)
+                imagemresposta.setImageResource(R.drawable.jeep)
             }
         }
         if(Variaveis.potencia == 0){
             if(Variaveis.beleza >= 4){
                 textoresposta.text = "Aston Martin Vantage Roadster"
-                imagemresposta.setImageResource(R.drawable.Aston)
+                imagemresposta.setImageResource(R.drawable.aston)
             }
             else if(Variaveis.custo_beneficio >= 4){
                 textoresposta.text = "Vai de busão que compensa mais!"
-                imagemresposta.setImageResource(R.drawable.Onibus)
+                imagemresposta.setImageResource(R.drawable.onibus)
             }
             else if(Variaveis.ze_da_manga >= 5){
                 textoresposta.text = "Você é o ZÉ DA MANGA!"
-                imagemresposta.setImageResource(R.drawable.Cario)
+                imagemresposta.setImageResource(R.drawable.cario)
             }
             else if(Variaveis.arraza_role >= 4){
                 textoresposta.text = "Carro de Baiano"
-                imagemresposta.setImageResource(R.drawable.Baiano)
+                imagemresposta.setImageResource(R.drawable.baiano)
             }
             else{
                 textoresposta.text = "Você é um ser Humano"
-                imagemresposta.setImageResource(R.drawable.Humano)
+                imagemresposta.setImageResource(R.drawable.humano)
             }
         }
 
+        val buttonWeb = findViewById<Button>(R.id.buttonGIT)
 
+        buttonWeb.setOnClickListener{
+            val webpage = Uri.parse("https://github.com/F4R1xx/AP2DesenvolvimentoMobile/tree/main/exemplointent")
+            val intent = Intent(Intent.ACTION_VIEW, webpage)
+            startActivity(intent)
+        }
 
 
 
